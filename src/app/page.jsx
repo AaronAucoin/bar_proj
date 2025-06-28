bb71dc86495c47141d1e9351bf2d01894b823be4
AaronAucoin <aauco2000@gmail.com>
2025-06-28 13:34:09 -0500

    Created Project

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Square from '../components/login/LoginPage.jsx';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Square />
+    </div>
+  )
+}
---
+++ src/components/login/LoginPage.jsx
@@ -0,0 +1,16 @@
+import './LoginPage.css';
+
+export default function LoginPage() {
+  return (
+    <div className="page-wrapper">
+      <div className="login-box">
+        <div className="Title">
+            Login
+        </div>
+        <div className="Bottom">
+            BottomPage
+        </div>
+      </div>
+    </div>
+  );
+}
